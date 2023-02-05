fef1a4a7a3aafe4faca41cab02183cbc2ff1ffad
Michael Chop <007micle007@gmail.com>
2023-02-05 21:45:36 +0300

    Added some styles to the project

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import {StreamChat} from 'stream-chat'
+import {Chat} from 'stream-chat-react'
+import Cookies from 'universal-cookie'
+
+import {ChannelListContainer, ChannelContainer} from './components'
+import './App.scss'
+
+const apiKey = 'kdcn597mad2v'
+
+const client = StreamChat.getInstance(apiKey)
 
 const App = () => {
     return (
-        <div>
-            <h1>Hello world!</h1>
+        <div className={"app_wrapper"}>
+            <Chat client={client} theme={"team light"}>
+                <ChannelListContainer
+
+                />
+                <ChannelContainer
+
+                />
+            </Chat>
         </div>
     );
 };
---
+++ client/src/components/ChannelListContainer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ChannelListContainer = () => {
+    return (
+        <div>
+            ChannelListContainer
+        </div>
+    );
+};
+
+export default ChannelListContainer;
