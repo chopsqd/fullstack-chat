12cdb96f78a63f57f99f2fd8966d73c04137e963
Michael Chop <007micle007@gmail.com>
2023-02-06 18:00:53 +0300

    Added a client-server communication to the project

--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -4,6 +4,8 @@ import axios from 'axios';
 
 import signInImage from '../assets/signup.jpg'
 
+const cookies = new Cookies()
+
 const initialState = {
     fullName: '',
     username: '',
@@ -28,8 +30,29 @@ const Auth = () => {
         setIsSignup((prevIsSignup) => !prevIsSignup)
     }
 
-    const handleSubmit = (event) => {
+    const handleSubmit = async (event) => {
         event.preventDefault()
+
+        const {fullName, username, password, phoneNumber, avatarURL} = form
+
+        const URL = 'http://localhost:5000/auth'
+
+        const { data: {token, userId, hashedPassword}} = await axios.post(`${URL}/${isSignup ? 'signup' : 'login'}`, {
+            username, password, fullName, phoneNumber, avatarURL
+        })
+
+        cookies.set('token', token)
+        cookies.set('username', username)
+        cookies.set('fullName', fullName)
+        cookies.set('userId', userId)
+
+        if(isSignup) {
+            cookies.set('phoneNumber', phoneNumber)
+            cookies.set('avatarURL', avatarURL)
+            cookies.set('hashedPassword', hashedPassword)
+        }
+
+        window.location.reload()
     }
 
     return (
--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,7 +1,8 @@
 const {connect} = require('getstream')
 const bcrypt = require('bcrypt')
-const {StreamChat} = require('stream-chat')
+const StreamChat = require('stream-chat').StreamChat
 const crypto = require('crypto')
+require('dotenv').config()
 
 const api_key = process.env.STREAM_API_KEY
 const api_secret = process.env.STREAM_API_SECRET
